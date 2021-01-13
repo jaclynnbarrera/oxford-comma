7ad1394ead56c9d61c399077556d79e51b0a563e
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-12 23:07:14 -0500

    commit tree d203910369cb3b99bcd72faeb9b4aeae9ce37e3a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join (" and ")
+    elsif array.length == 3
+        array[0..-2].join(", ") + ", and " + array[-1]
+    elsif array.length > 3
+        last = array.pop
+        array.join(", ") + ", and " + last.to_s
+    end 
 end
